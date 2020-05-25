71f303998531dd7d7be0d579587f45286bffbc16
kktk-KO <noreply>
2020-05-26 00:34:54 +0900

    add Message and serialization

--- src/message.rs
+++ src/message.rs
@@ -2,6 +2,7 @@ use std::time::Instant;
 use std::fmt::Formatter;
 use std::fmt::Display;
 use std::fmt::Result;
+use std::io::Write;
 
 pub enum MessageType {
     Lock,
@@ -24,8 +25,31 @@ impl Display for MessageType {
     }
 }
 
+impl MessageType {
+    pub fn to_i32 (&self) -> i32 {
+        match self {
+            MessageType::Lock => 0,
+            MessageType::UnLock => 1,
+        }
+    }
+}
+
 impl Display for Message {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "message_type = {}, tid = {}", self.message_type, self.tid)
+    }
+}
+
+impl Message {
+    pub fn write<T>(&self, f: &mut T, t0: &Instant)
+        where T: Write
+    {
+        let t1 = self.start_time.duration_since(*t0).as_nanos();
+        let t2 = self.finish_time.duration_since(*t0).as_nanos();
+        assert_eq!(f.write(&t1.to_be_bytes()).unwrap(), 16);
+        assert_eq!(f.write(&t2.to_be_bytes()).unwrap(), 16);
+        assert_eq!(f.write(&self.tid.to_be_bytes()).unwrap(), 4);
+        assert_eq!(f.write(&self.message_type.to_i32().to_be_bytes()).unwrap(), 4);
+        f.flush().unwrap();
     }
 }
--- src/recorder.rs
+++ src/recorder.rs
@@ -1,11 +1,12 @@
 use std::thread::spawn;
 use std::thread::JoinHandle;
-use lazy_static::lazy_static;
 use std::sync::mpsc::SyncSender;
 use std::sync::mpsc::Receiver;
 use std::sync::mpsc::sync_channel;
+use std::fs::File;
+use std::io::BufWriter;
+use lazy_static::lazy_static;
 use crate::message::Message;
-use crate::debug::print_debug;
 use crate::activate::activate;
 
 struct Recorder {
@@ -33,18 +34,13 @@ fn worker(receiver: Receiver<Message>) {
     let message = receiver.recv().unwrap();
     // TODO: is there such guarantee that the first message is generated first???
     let t0 = message.start_time;
-    print_debug(format!("{} start_time = {} finish_time = {} \n",
-        message,
-        message.start_time.duration_since(t0).as_nanos(),
-        message.finish_time.duration_since(t0).as_nanos(),
-    ));
 
+    let mut f = BufWriter::new(File::create("mutrack.data").unwrap());
+
+    message.write(&mut f, &t0);
     loop {
         let message = receiver.recv().unwrap();
-        print_debug(format!("{} start_time = {} finish_time = {} \n",
-            message,
-            message.start_time.duration_since(t0).as_nanos(),
-            message.finish_time.duration_since(t0).as_nanos(),
-        ));
+        message.write(&mut f, &t0);
     }
 }
+
